5e317d60fc362503cfc07c8ffc432308646756f4
Hangil Chang <hangil@chang.jp>
2025-08-03 14:51:50 +0900

    fixed home href

--- build.py
+++ build.py
@@ -9,7 +9,12 @@ site = env.get_template('site.html')
 page_files = glob.glob("templates/pages/*.html")
 for page_file in page_files:
     page_content = frontmatter.load(page_file)
-    page = site.render(title=page_content["title"], content=page_content)
+    home = "./" if page_content["title"] == "index" else "../"
+    page = site.render(
+        title=page_content["title"], 
+        home=home,
+        content=page_content
+        )
     page_name = os.path.basename(page_file)
     with open(f"docs/{page_name}", "w", encoding="utf-8") as f:
         f.write(page)
